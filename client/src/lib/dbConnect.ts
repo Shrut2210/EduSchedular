b61514e329c340a64c0e5dbaa9be2768cbc6d9a2
madhav <109799001+madhav037@users.noreply.github.com>
2024-11-10 21:06:31 +0530

    signup login logout and protected routes done

--- client/src/lib/dbConnect.ts
+++ client/src/lib/dbConnect.ts
@@ -1,6 +1,7 @@
-import { createClient } from '@supabase/supabase-js'
+import { Database } from '../../types/supabase';
+import { createClient } from '@supabase/supabase-js';
 
-const supabase = createClient(
-  process.env.SUPABASEURL || '',
-  process.env.SUPABASEKEY || ''
-)
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || '';
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || '';
+
+export const supabase = createClient<Database>(supabaseUrl, supabaseAnonKey);
